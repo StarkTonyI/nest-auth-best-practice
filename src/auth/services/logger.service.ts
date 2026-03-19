afe9a5f33eecebf29d9f90d0b818160446892192
Mansur <entonyam95@gmail.com>
2026-03-19 22:39:47 +0500

    add logger service, methods: logger, err, warning; accepting message, context, method, module

---
+++ src/auth/services/logger.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, Logger } from "@nestjs/common";
+import { ApiConfigServices } from "src/configService/apiConfig.service";
+
+export class Context {
+    module: string;
+    method: string;
+}
+@Injectable()
+export class LoggerService extends Logger {
+    constructor(private readonly config: ApiConfigServices){super()}
+
+    logger(message: string, context: Context){
+        const standtart = {
+            message,
+            host: this.config.portConfig,
+            type: 'INFO',
+            timeStamp: new Date().toISOString()
+        }
+        const data = { standtart, ...context }
+        return super.log(data)
+    }
+
+    err(message: string, context: Context){
+        const standtart = {
+            message,
+            host: this.config.portConfig,
+            type: 'ERROR',
+            timeStamp: new Date().toISOString()
+        }
+        const data = { standtart, ...context }
+        return super.error(data)
+    }
+
+    warning(message: unknown, context: Context): void {
+        const standtart = {
+            message,
+            host: this.config.portConfig,
+            type: 'WARNING',
+            timeStamp: new Date().toISOString()
+        }
+        const data = { standtart, ...context }
+        return super.warn(data)
+    }
+
+}
+
+
+
+
+
+
+
+
+
